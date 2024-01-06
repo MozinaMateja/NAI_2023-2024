a6136f7743d8cb01bef408966532a4eb1ab4896e
MozinaMateja <64915911+MozinaMateja@users.noreply.github.com>
2024-01-06 19:24:07 +0100

    ...

---
+++ testTRIGO.m
@@ -0,0 +1,17 @@
+format long
+f = @(x) (cos(x)).^4 + exp(-x);
+a = 0;
+n = 4;
+b = 4*pi*n./(2*n+1);
+
+tocke_napake = linspace(0, 2*pi*(2*n)/(2*n+1), 201);
+[y, koef] = TrigonometricnaInterpolacija(f, a, b, n, tocke_napake);
+koef         %koeficienti trig.poli.
+norm(arrayfun(f,tocke_napake) - y, 'inf')    %najvecja napaka
+
+tocke_napake2 = linspace(0,5,201);
+[y2, koef2] = TrigonometricnaInterpolacija(f, 0, 5, n, tocke_napake2);
+koef2        %koeficienti trig.poli.
+norm(arrayfun(f,tocke_napake2) - y2, 'inf')    %najvecja napaka
+p = polyfit(tocke_napake2,arrayfun(f,tocke_napake2),2*n);
+norm(arrayfun(f,tocke_napake2) - polyval(p,tocke_napake2), 'inf')
