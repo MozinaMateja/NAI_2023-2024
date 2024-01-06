cdae68df58ddfa12e8e3c7e07223fe7b39d04a14
MozinaMateja <64915911+MozinaMateja@users.noreply.github.com>
2024-01-06 20:19:31 +0100

    Popravljen testTRIGO

--- testTRIGO.m
+++ testTRIGO.m
@@ -2,7 +2,7 @@ format long
 f = @(x) (cos(x)).^4 + exp(-x);
 a = 0;
 n = 4;
-b = 4*pi*n./(2*n+1);
+b = 5; %4*pi*n./(2*n+1);
 
 tocke_napake = linspace(0, 2*pi*(2*n)/(2*n+1), 201);
 [y, koef] = TrigonometricnaInterpolacija(f, a, b, n, tocke_napake);
@@ -13,5 +13,7 @@ tocke_napake2 = linspace(0,5,201);
 [y2, koef2] = TrigonometricnaInterpolacija(f, 0, 5, n, tocke_napake2);
 koef2        %koeficienti trig.poli.
 norm(arrayfun(f,tocke_napake2) - y2, 'inf')    %najvecja napaka
-p = polyfit(tocke_napake2,arrayfun(f,tocke_napake2),2*n);
+
+x = linspace(a, b, 2*n+1);
+p = polyfit(x,arrayfun(f,x),2*n);
 norm(arrayfun(f,tocke_napake2) - polyval(p,tocke_napake2), 'inf')
